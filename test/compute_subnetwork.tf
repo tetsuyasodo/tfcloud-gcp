dd30cd736aec051451cdfc8c7e2fbf529772ff4d
Tetsuya Sodo <tetsuya.sodo@gmail.com>
2021-01-05 16:43:00 +0900

    Update compute_subnetwork.tf

--- test/compute_subnetwork.tf
+++ test/compute_subnetwork.tf
@@ -3,6 +3,7 @@ resource "google_compute_subnetwork" "pub_subnetwork" {
   ip_cidr_range = "192.168.1.0/24"
   region        = "asia-northeast1"
   network       = "sample-network"
+  depends_on    = [google_compute_network.sample_network]
 }
 
 resource "google_compute_subnetwork" "prv_subnetwork" {
@@ -10,4 +11,5 @@ resource "google_compute_subnetwork" "prv_subnetwork" {
   ip_cidr_range = "10.0.1.0/24"
   region        = "asia-northeast3"
   network       = "sample-network"
+  depends_on    = [google_compute_network.sample_network]
 }
