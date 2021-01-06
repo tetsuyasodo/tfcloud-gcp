1c86eabb8bf723dd4b806a7e0f37575a90e47f96
Tetsuya Sodo <tetsuya.sodo@gmail.com>
2021-01-06 22:05:47 +0900

    Update compute_firewall.tf

--- compute_firewall.tf
+++ compute_firewall.tf
@@ -30,7 +30,7 @@ resource "google_compute_firewall" "sample_network_allow_http" {
 
   allow {
     protocol = "tcp"
-    ports    = ["80"]
+    ports    = ["5601"]
   }
 
   source_ranges = ["0.0.0.0/0"]
