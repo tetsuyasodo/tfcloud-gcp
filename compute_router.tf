9baa4a902b1b29469ca3d450500bdfe4e0c959a1
Tetsuya Sodo <tetsuya.sodo@gmail.com>
2021-01-11 11:01:21 +0900

    delete old files

--- compute_router.tf
+++
@@ -1,9 +0,0 @@
-resource "google_compute_router" "seoul_router" {
-  name    = "seoul-router"
-  region  = "asia-northeast3"
-  network = google_compute_network.sample_network.self_link
-
-  bgp {
-    asn = 64514
-  }
-}
--- install_nginx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-apt update
-apt install -y nginx
-cat <<EOF >/var/www/html/index.html
-<HTML><BODY>
-<h1>Hello world.<h1>
-</BODY></HTML>
-EOF
