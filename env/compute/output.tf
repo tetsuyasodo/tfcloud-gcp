66d51a0306424e8bab84c956746997e76efddb17
Tetsuya Sodo <tetsuya.sodo@gmail.com>
2021-01-19 14:46:27 +0900

    Update output.tf

--- env/compute/output.tf
+++ env/compute/output.tf
@@ -1,11 +1,14 @@
 output "bastion-ip" {
-  value = google_compute_instance.bastion.network_interface.0.access_config.0.nat_ip
+  count = length(var.users)
+  value = google_compute_instance.bastion[count.index].network_interface.0.access_config.0.nat_ip
 }
 
 output "mon-ip" {
-  value = google_compute_instance.mon01.network_interface.0.access_config.0.nat_ip
+  count = length(var.users)
+  value = google_compute_instance.mon01[count.index].network_interface.0.access_config.0.nat_ip
 }
 
 output "kb01-ip" {
-  value = google_compute_instance.kb01.network_interface.0.access_config.0.nat_ip
+  count = length(var.users)
+  value = google_compute_instance.kb01[count.index].network_interface.0.access_config.0.nat_ip
 }
