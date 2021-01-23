5081ee126eba32a0d9ee748129c770d394619f00
Tetsuya Sodo <tetsuya.sodo@gmail.com>
2021-01-23 12:30:18 +0900

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
-output "comp-bastion-ip" {
+output "bastion-ip" {
   value = module.env-compute.bastion-ip
+}
+
+output "mon-ip" {
+  value = module.env-compute.mon-ip
+}
+
+output "kb01-ip" {
+  value = module.env-compute.kb01-ip
 }
