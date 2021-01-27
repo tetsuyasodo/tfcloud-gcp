d7512cee381aa64185aad254e8e793e76d752897
Tetsuya Sodo <tetsuya.sodo@gmail.com>
2021-01-28 00:03:52 +0900

    Update install_es.sh

--- scripts/install_es.sh
+++ scripts/install_es.sh
@@ -24,9 +24,25 @@ cluster.name: cluster$(hostname | awk -Fuser '{print $2}')
 network.host: 0.0.0.0
 discovery.seed_hosts: ["${HOSTNAME}"]
 cluster.initial_master_nodes:  ["${HOSTNAME}"]
+
+# 3ノードクラスタ構成の設定
 #discovery.seed_hosts: ["es01-$(hostname | awk -F- '{print $2}')", "es02-$(hostname | awk -F- '{print $2}')", "es03-$(hostname | awk -F- '{print $2}')"]
 #cluster.initial_master_nodes: ["es01-$(hostname | awk -F- '{print $2}')", "es02-$(hostname | awk -F- '{print $2}')", "es03-$(hostname | awk -F- '{print $2}')"]
+
+# Security機能の有効化
+#xpack.security.enabled: true
+
+# ノード間のTLS通信暗号化の有効化
+#xpack.security.transport.ssl.enabled: true
+#xpack.security.transport.ssl.verification_mode: certificate
+#xpack.security.transport.ssl.keystore.path: elastic-certificates.p12
+#xpack.security.transport.ssl.truststore.path: elastic-certificates.p12
 EOF
+
+yum install -y wget
+wget https://github.com/tetsuyasodo/tfcloud-gcp/raw/main/scripts/elastic-certificates.p12 -O /etc/elasticsearch/elastic-certificates.p12
+chown root:elasticsearch /etc/elasticsearch/elastic-certificates.p12
+chmod 660 /etc/elasticsearch/elastic-certificates.p12
 
 systemctl daemon-reload
 #systemctl enable elasticsearch
